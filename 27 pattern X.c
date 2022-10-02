67c0d90072b54460b7d0596a55493510f1bed709
AdarshSuryawansh <103188189+AdarshSuryawansh@users.noreply.github.com>
2022-10-02 16:39:54 +0530

    Add files via upload

---
+++ 27 pattern X.c
@@ -0,0 +1,27 @@
+#include<stdio.h>
+#include<conio.h>
+int main()
+{
+    int r=0,c=0,cnt=0;
+
+    printf("\n Enter size to print pattern =>");
+    scanf("%d",&cnt);
+
+    for(r=1;r<=cnt;r++)
+    {
+        for(c=1;c<=cnt;c++)
+        {
+            if(r==c || r+c==cnt+1)
+            {
+                printf(" * ");
+            }
+            else
+            {
+                printf("   ");
+            }
+        }
+                printf("\n");
+    }
+             getch();
+             return 0;
+}
---
+++ 32 Pattern S.c
@@ -0,0 +1,27 @@
+#include<stdio.h>
+#include<conio.h>
+int main()
+{
+    int r=0, c=0, cnt=0;
+
+    printf("\n Enter size to print pattern =>");
+    scanf("%d",&cnt);
+
+    for(r=1;r<=cnt;r++)
+         {
+             for(c=1;c<=cnt;c++)
+             {
+                 if()
+                 {
+                     printf(" * ");
+                 }
+                 else
+                 {
+                     printf("   ");
+                 }
+             }
+                    printf("\n");
+         }
+         getch();
+         return 0;
+}
