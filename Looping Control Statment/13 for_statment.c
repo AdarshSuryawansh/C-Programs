a6746843ae1a4a919b96d0b678dbc9e7af914995
AdarshSuryawansh <103188189+AdarshSuryawansh@users.noreply.github.com>
2022-10-02 16:49:32 +0530

    Add files via upload

---
+++ Looping Control Statment/13 for_statment.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+#include<conio.h>
+int main()
+{
+    int i=10;
+
+    for(i=1; i<=10; i++)
+    {
+        printf("\n I am Indian");
+    }
+
+    printf("\n%d",i);
+
+    getch();
+    return 0;
+}
---
+++ Looping Control Statment/14 do_while statment.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+#include<conio.h>
+int main()
+{
+    int i=1;
+
+    do
+    {
+        printf("\n INDIA");
+        printf("\n Value of Variable= %d,i");
+        i++;
+
+    }while(i<=10);
+
+    printf("\n Thanks!!!");
+
+    getch();
+    return 0;
+}
