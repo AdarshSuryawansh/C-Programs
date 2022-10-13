f7362c3106bb1d92db6fa6510d8ef35ca85c53bc
AdarshSuryawansh <103188189+AdarshSuryawansh@users.noreply.github.com>
2022-10-13 12:57:11 +0530

    Add files via upload

---
+++ Conditional Statements & Loops/21 Maximum From Two Numbers_Using else if ladder.c
@@ -0,0 +1,32 @@
+
+
+//Compiler version gcc  6.3.0
+#include<stdio.h>
+#include<conio.h>
+
+int main()
+{
+  int Num1 = 0,Num2 = 0;
+  
+  printf("\n Enter First Number = ");
+  scanf("%d",&Num1);
+  printf("\n Enter Second Number = ");
+  scanf("%d",&Num2);
+  
+  if(Num1 == Num2)
+  {
+     printf("\n Both Numbers Are Equal");
+  }
+  else if(Num1 > Num2)
+  {
+     printf("\n Number One = %d is Maximum",Num1);
+  }
+  else
+  {
+     printf("\n Number Two = %d is Maximum",Num2);
+  }
+  printf("\n\n Thanks !!!");
+  
+  getch();
+  return 0;
+}
