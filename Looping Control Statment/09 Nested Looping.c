1d0d17b6d127aab5e8a9c43b8121c67296d2f0fe
AdarshSuryawansh <103188189+AdarshSuryawansh@users.noreply.github.com>
2022-10-18 13:51:19 +0530

    Add files via upload

---
+++ Looping Control Statment/09 Nested Looping.c
@@ -0,0 +1,26 @@
+
+
+//Compiler version gcc  6.3.0
+
+#include<stdio.h>
+#include<conio.h>
+
+int main()
+{
+    int i = 0,j = 0;
+
+    printf("\n\t Welcome To Class \n");
+
+    for(i = 1;i <= 4;i ++)
+    {
+        for(j =1;j <=5;j ++)
+        {
+            printf("\n\t Fork Infosystems");
+        }
+        printf("\n\t Karad \n");
+    }
+    printf("\n\t Thanks !!!!");
+
+    getch();
+    return 0;
+}
