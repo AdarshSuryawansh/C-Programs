740b6527fd727d2ed0310107ff04e2b28e8b1e16
AdarshSuryawansh <103188189+AdarshSuryawansh@users.noreply.github.com>
2022-10-23 11:36:45 +0530

    Add files via upload

---
+++ Pattern printing/14 B_Pattern_Printing.c
@@ -0,0 +1,37 @@
+
+
+//Compiler version gcc  6.3.0
+#include<stdio.h>
+#include<conio.h>
+
+int main()
+{
+         int r = 0, c = 0, Cnt = 0;
+
+         printf("\n Enter Count Of Rows and Columns = ");
+         scanf("%d",&Cnt);
+
+         printf("\n pattern is =>\n\n");
+
+         for(r = 1; r <= Cnt; r++)
+         {
+                  for(c = 1; c <= Cnt; c++)
+                  {
+                          if(r == 1 && c < Cnt || c == 1 || r == (Cnt+1)/2 && c < Cnt || r == Cnt && c < Cnt || c == Cnt && r < Cnt && r != (Cnt+1)/2 && r > 1 )
+                          {
+                                printf(" *");
+                          }
+                          else
+                          {
+                                printf("  ");
+                          }
+                  }
+                  printf("\n");
+         }
+
+         printf("\n\n Thanks !!!\n");
+
+         getch();
+         return 0;
+}
+
