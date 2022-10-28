a123577d3d218540e08935ca5d767b53f42a860f
AdarshSuryawansh <103188189+AdarshSuryawansh@users.noreply.github.com>
2022-10-28 21:58:54 +0530

    Add files via upload

---
+++ Pattern printing/27 O_Pattern_Printing.c
@@ -0,0 +1,36 @@
+
+
+//Compiler version gcc  6.3.0
+#include<stdio.h>
+#include<conio.h>
+
+int main()
+{
+         int r = 0, c = 0, Cnt = 0;
+
+         printf("\n Enter Count For Rows and Columns = ");
+         scanf("%d",&Cnt);
+
+         printf("\n\n pattern is =>\n\n");
+
+         for(r = 1; r <= Cnt; r++)
+         {
+                  for(c = 1; c <= Cnt; c++)
+                  {
+                           if( c==1 && r > 1 && r < Cnt || r== 1 && c < Cnt && c > 1 || c == Cnt && r > 1 && r < Cnt || r == Cnt && c < Cnt && c > 1)
+                           {
+                                 printf(" *");
+                           }
+                           else
+                           {
+                                 printf("  ");
+                           }
+                  }
+                  printf("\n");
+         }
+
+         printf("\n\n Thanks !!!\n");
+
+         getch();
+         return 0;
+}
