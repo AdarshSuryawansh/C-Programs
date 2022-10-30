84195b1688b7136295ddca3eb4e638b67e2894f5
AdarshSuryawansh <103188189+AdarshSuryawansh@users.noreply.github.com>
2022-10-30 21:30:06 +0530

    Add files via upload

---
+++ Array/09 Array_E.c
@@ -0,0 +1,22 @@
+
+
+//Compiler version gcc  6.3.0
+
+#include<stdio.h>
+#include<conio.h>
+
+int main()
+{
+        char Grade[5] = {'A', 'B', 'C', 'D', 'E'};
+
+        printf("\n\n The Grade Of 1st Candidate = %c",Grade[0]);
+        printf("\n\n The Grade Of 2nd Candidate = %c",Grade[1]);
+        printf("\n\n The Grade Of 3rd Candidate = %c",Grade[2]);
+        printf("\n\n The Grade Of 4th Candidate = %c",Grade[3]);
+        printf("\n\n The Grade Of 5th Candidate = %c",Grade[4]);
+
+        printf("\n\n Thanks !!!\n");
+
+        getch();
+        return 0;
+}
