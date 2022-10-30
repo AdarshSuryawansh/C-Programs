26e05cadf6294e7efe9b1909f6b47e92589b40d8
AdarshSuryawansh <103188189+AdarshSuryawansh@users.noreply.github.com>
2022-10-30 21:25:57 +0530

    Add files via upload

---
+++ Array/05 Array_A.c
@@ -0,0 +1,22 @@
+
+
+//Compiler version gcc  6.3.0
+#include<stdio.h>
+#include<conio.h>
+
+int main()
+{
+        int Num[6] = {12, 23, 54, 67, 23, 65};
+
+        printf("\n\n The Value Of 1st Number = %d",Num[0]);
+        printf("\n\n The Value Of 2nd Number = %d",Num[1]);
+        printf("\n\n The Value Of 3rd Number = %d",Num[2]);
+        printf("\n\n The Value Of 4th Number = %d",Num[3]);
+        printf("\n\n The Value Of 5th Number = %d",Num[4]);
+        printf("\n\n The Value Of 6th Number = %d",Num[5]);
+
+        printf("\n\n Thanks !!!\n");
+
+        getch();
+        return 0;
+}
