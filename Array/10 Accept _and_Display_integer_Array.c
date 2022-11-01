25bc6aec0038f6062a6b2d7ffc47833b47d83cb1
AdarshSuryawansh <103188189+AdarshSuryawansh@users.noreply.github.com>
2022-11-01 18:24:49 +0530

    Add files via upload

---
+++ Array/10 Accept _and_Display_integer_Array.c
@@ -0,0 +1,27 @@
+
+
+//Compiler version gcc  6.3.0
+
+#include<stdio.h>
+#include<conio.h>
+
+int main()
+{
+        int Marks[5] = {'\0'};
+
+        printf("\n\n Enter Marks Of 1st Student = ");
+        scanf("%d",&Marks[0]);
+        printf("\n\n Enter Marks Of 1st Student = ");
+        scanf("%d",&Marks[1]);
+        printf("\n\n Enter Marks Of 2nd Student = ");
+        scanf("%d",&Marks[2]);
+        printf("\n\n Enter Marks Of 3rd Student = ");
+        scanf("%d",&Marks[3]);
+        printf("\n\n Enter Marks Of 4th Student = ");
+        scanf("%d",&Marks[4]);
+
+        printf("\n\n Thanks !!! \n");
+
+        getch();
+        return 0;
+}
