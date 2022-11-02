d0d6e21cf8a3df0c404e248b56fb81229e562bfd
AdarshSuryawansh <103188189+AdarshSuryawansh@users.noreply.github.com>
2022-11-02 20:01:44 +0530

    Add files via upload

---
+++ Array/15 Zero_Count_In_Array.c
@@ -0,0 +1,33 @@
+
+
+//Compiler version gcc  6.3.0
+
+#include<stdio.h>
+#include<conio.h>
+
+int main()
+{
+        int Arr[7] = {'\0'}, i = 0, Z_Cnt = 0;
+
+        printf("\n\n Enter Array Elements For Finding Zero Count.");
+        for(i = 0; i < 7; i++)
+        {
+                printf("\n\n Enter %d Element = ",i+1);
+                scanf("%d",&Arr[i]);
+        }
+
+        for(i = 0; i < 7; i++)
+        {
+                if(Arr[i] == 0)
+                {
+                    Z_Cnt++;
+                }
+        }
+        printf("\n\n The Count Of Zero in Given Array = %d",Z_Cnt);
+
+        printf("\n\n Thanks !!!\n");
+
+        getch();
+        return 0;
+
+}
