0bcf97a20d6f69cde4d4774133d58f02e059bcc6
AdarshSuryawansh <103188189+AdarshSuryawansh@users.noreply.github.com>
2022-11-04 13:06:34 +0530

    Add files via upload

---
+++ String/09 String_gets_puts.c
@@ -0,0 +1,20 @@
+
+
+//Compiler version gcc  6.3.0
+#include<stdio.h>
+#include<conio.h>
+
+int main()
+{
+        char cSrc[50] = {'\0'};
+
+        puts("\n Enter a String = ");
+        gets(cSrc);
+
+        printf("\n\n Entered String is = %s",cSrc);
+
+        printf("\n\n Thanks !!!\n");
+
+        getch();
+        return 0;
+}
