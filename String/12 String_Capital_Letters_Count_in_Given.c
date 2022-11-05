756b4eaa158b24eaae30f1fa78c9790b91caec5f
AdarshSuryawansh <103188189+AdarshSuryawansh@users.noreply.github.com>
2022-11-05 19:15:42 +0530

    Add files via upload

---
+++ String/12 String_Capital_Letters_Count_in_Given.c
@@ -0,0 +1,31 @@
+
+
+//Compiler version gcc  6.3.0
+
+#include<stdio.h>
+#include<conio.h>>
+
+int main()
+{
+        char cSrc[50] = {'\0'};
+        int Cnt = 0, i = 0;
+
+        printf("\n Enter a String = ");
+        gets(cSrc);
+
+        while(cSrc[i] != '\0')
+        {
+                if(cSrc[i] >= 'A' && cSrc[i] <= 'Z')
+                {
+                    Cnt++;
+                }
+                i++;
+        }
+
+        printf("\n The Capital Letters Count in Given String = %d",Cnt);
+
+        printf("\n\n Thanks !!!\n");
+
+        getch();
+        return 0;
+}
