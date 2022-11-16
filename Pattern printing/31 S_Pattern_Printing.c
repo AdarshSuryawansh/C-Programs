4b8626fadc26f5e18324ec02629db1ecc9a93162
AdarshSuryawansh <103188189+AdarshSuryawansh@users.noreply.github.com>
2022-11-16 22:57:21 +0530

    Add files via upload

---
+++ Pattern printing/31 S_Pattern_Printing.c
@@ -0,0 +1,36 @@
+
+
+//Compiler version gcc  6.3.0
+#include<stdio.h>
+#include<conio.h>
+
+int main()
+{
+         int r = 0, c = 0, Cnt = 0;
+
+         printf("\n Enter Odd Number For Count Of Rows and Columns = ");
+         scanf("%d",&Cnt);
+
+         printf("\n\n Pattern is =>\n\n");
+
+         for(r = 1; r <= Cnt; r++)
+         {
+                  for(c = 1; c <= Cnt; c++)
+                  {
+                           if(r == 1 && c != 1 || c == 1 && r < (Cnt+1)/2 && r != 1 || r == (Cnt+1)/2 && c < Cnt && c > 1 || c == Cnt && r > (Cnt+1)/2 && r < Cnt || r == Cnt && r > c)
+                           {
+                                 printf(" *");
+                           }
+                           else
+                           {
+                                 printf("  ");
+                           }
+                  }
+                  printf("\n");
+         }
+
+         printf("\n\n Thanks !!!\n");
+
+         getch();
+         return 0;
+}
