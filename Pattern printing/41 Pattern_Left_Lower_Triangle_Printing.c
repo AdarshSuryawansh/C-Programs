9a5d16ae085a0b22acd0e627e48d1e4c9783613c
AdarshSuryawansh <103188189+AdarshSuryawansh@users.noreply.github.com>
2022-11-26 22:23:55 +0530

    Add files via upload

---
+++ Pattern printing/41 Pattern_Left_Lower_Triangle_Printing.c
@@ -0,0 +1,32 @@
+
+
+//Compiler version gcc  6.3.0
+#include<stdio.h>
+#include<conio.h>
+
+int main()
+{
+         int r = 0, c = 0, Cnt = 0;
+
+         printf("\n Enter Count Of Rows and Columns = ");
+         scanf("%d",&Cnt);
+
+         printf("\n\n pattern is =>\n\n");
+
+         for(r = 1; r <= Cnt; r++)
+         {
+                  for(c = 1; c <= Cnt; c++)
+                  {
+                           if(r >= c)
+                           {
+                                 printf(" * ");
+                           }
+                  }
+                  printf("\n");
+         }
+
+         printf("\n\n Thanks !!!\n");
+
+         getch();
+         return 0;
+}
