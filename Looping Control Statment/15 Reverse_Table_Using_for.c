306ea8042fc96ba15ff7c3ac566772366e7af66c
AdarshSuryawansh <103188189+AdarshSuryawansh@users.noreply.github.com>
2022-12-05 19:40:15 +0530

    Add files via upload

---
+++ Looping Control Statment/15 Reverse_Table_Using_for.c
@@ -0,0 +1,28 @@
+
+
+//Compiler version gcc  6.3.0
+#include<stdio.h>
+#include<conio.h>
+
+int main()
+{
+       int Num = 0 ,Cnt = 0;
+
+       printf("\n\n\t=============******************=============\n");
+
+       printf("\n\t Enter Number To Print Its Table = ");
+       scanf("%d",&Num);
+
+       printf("\n\n\t=============******************=============\n");
+
+       for(Cnt=10 ; Cnt>=1 ; Cnt--)
+       {
+             printf("\n\t %3d * %3d = %3d",Num,Cnt,Num * Cnt);
+       }
+
+       printf("\n\n\t=============*******************=============\n");
+       printf("\n\t\t Thanks !!!\n");
+
+       getch();
+       return 0;
+}
