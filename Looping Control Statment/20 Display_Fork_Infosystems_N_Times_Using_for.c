0444f1774c518b526a154d2a58cc9537bd92ee10
AdarshSuryawansh <103188189+AdarshSuryawansh@users.noreply.github.com>
2022-12-10 23:26:54 +0530

    Add files via upload

---
+++ Looping Control Statment/20 Display_Fork_Infosystems_N_Times_Using_for.c
@@ -0,0 +1,29 @@
+
+
+//Compiler version gcc  6.3.0
+#include<stdio.h>
+#include<conio.h>
+
+int main()
+{
+         int Num = 0, Cnt = 0;
+
+         printf("\n\t Enter Positive Integer Number For Printing Name Of Class = ");
+         scanf("%d",&Num);
+
+         system("cls");
+         printf("\n\n\t============*********===============\n");
+
+         for(Cnt = 1; Cnt <= Num; Cnt++)
+         {
+               printf("\n\t\t FORK INFOSYSTEMS\n");
+         }
+
+         printf("\n\n\t============*********===============\n");
+         printf("\n\t\t Thanks !!!\n");
+
+         getch();
+         return 0;
+}
+
+
