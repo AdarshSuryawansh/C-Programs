54110dbe1e96ef249b7682f2a9f57100da8635b9
AdarshSuryawansh <103188189+AdarshSuryawansh@users.noreply.github.com>
2022-12-19 23:07:36 +0530

    Add files via upload

---
+++ Looping Control Statment/27 Factorial_Of_Number_Using_Short_Hand.c
@@ -0,0 +1,22 @@
+
+
+//Compiler version gcc  6.3.0
+#include<stdio.h>
+#include<conio.h>
+
+int main()
+{
+         int Num = 0, Temp = 0, Fact = 0;
+
+         printf("\n\t Enter a Number For Finding Factorial = ");
+         scanf("%d",&Num);
+
+         for(Fact = 1, Temp = Num ; Temp > 1 ; Fact *= Temp, Temp--);
+
+         printf("\n\t Factorial of %d = %d",Num,Fact);
+
+         printf("\n\n\t Thanks !!!\n");
+
+         getch();
+         return 0;
+}
