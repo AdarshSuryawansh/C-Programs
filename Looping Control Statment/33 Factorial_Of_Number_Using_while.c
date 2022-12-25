b00df4a32ce3af0273f0d80249b385fe8bee5254
AdarshSuryawansh <103188189+AdarshSuryawansh@users.noreply.github.com>
2022-12-25 22:14:22 +0530

    Add files via upload

---
+++ Looping Control Statment/33 Factorial_Of_Number_Using_while.c
@@ -0,0 +1,28 @@
+
+
+//Compiler version gcc  6.3.0
+#include<stdio.h>
+#include<conio.h>
+
+int main()
+{
+         int Num = 0, Temp = 0, Fact = 0;
+
+         printf("\n\t Enter a Number For Finding Factorial = ");
+         scanf("%d",&Num);
+
+         Fact = 1;
+         Temp = Num;
+
+         while(Temp>1)
+         {
+               Fact = Fact*Temp;
+               Temp--;
+         }
+         printf("\n\t Factorial of %d = %d",Num,Fact);
+
+         printf("\n\n\t Thanks !!!\n");
+
+         getch();
+         return 0;
+}
