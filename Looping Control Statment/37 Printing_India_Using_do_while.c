eb4e197504d633ef933bc536935b0f732740fb84
AdarshSuryawansh <103188189+AdarshSuryawansh@users.noreply.github.com>
2023-01-08 22:36:38 +0530

    Add files via upload

---
+++ Looping Control Statment/37 Printing_India_Using_do_while.c
@@ -0,0 +1,24 @@
+
+
+//Compiler version gcc  6.3.0
+#include<stdio.h>
+#include<conio.h>
+
+int main()
+{
+         int i = 1;
+
+         printf("\n\n\t=========********=========\n");
+         do
+         {
+              printf("\n\t\t India");
+              i++;
+         }while(i<=5);
+
+         printf("\n\n\t=========********=========\n");
+
+         printf("\n\n\t Thanks !!!\n");
+
+         getch();
+         return 0;
+}
