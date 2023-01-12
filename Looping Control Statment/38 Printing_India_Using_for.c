079982bd95cd5370f4ca39eb9b3d36cf26169f06
AdarshSuryawansh <103188189+AdarshSuryawansh@users.noreply.github.com>
2023-01-12 22:47:37 +0530

    Add files via upload

---
+++ Looping Control Statment/38 Printing_India_Using_for.c
@@ -0,0 +1,24 @@
+
+
+//Compiler version gcc  6.3.0
+#include<stdio.h>
+#include<conio.h>
+
+int main()
+{
+         int i = 0;
+
+         printf("\n\n\t=========********=========\n");
+
+         for(i=1 ; i<=5; i++)
+         {
+               printf("\n\t India");
+               printf("\n\t I Love My India\n");
+         }
+
+         printf("\n\n\t=========********=========\n");
+         printf("\n\n\t Thanks !!!\n");
+
+         getch();
+         return 0;
+}
