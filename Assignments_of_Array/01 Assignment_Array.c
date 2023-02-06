ee7a4cac448e2ebda55e11aed4eb8a2f94ce4d8a
AdarshSuryawansh <103188189+AdarshSuryawansh@users.noreply.github.com>
2023-02-06 21:32:31 +0530

    Add files via upload

---
+++ Assignments_of_Array/01 Assignment_Array.c
@@ -0,0 +1,22 @@
+
+
+//Compiler version gcc  6.3.
+/// 01.Create Array Without Initialization & Display All Elements.
+#include<stdio.h>
+#include<conio.h>
+
+int main()
+{
+        int Arr[5];
+
+        printf("\n\n The 1st Element = %d",Arr[0]);
+        printf("\n\n The 2nd Element = %d",Arr[1]);
+        printf("\n\n The 3rd Element = %d",Arr[2]);
+        printf("\n\n The 4th Element = %d",Arr[3]);
+        printf("\n\n The 5th Element = %d",Arr[4]);
+
+        printf("\n\n Thanks !!!\n");
+
+        getch();
+        return 0;
+}
