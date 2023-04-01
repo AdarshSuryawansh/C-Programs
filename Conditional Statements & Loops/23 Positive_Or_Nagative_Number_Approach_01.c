6ddd1f50ccf7b104d79efdef83101d10089fb8d9
AdarshSuryawansh <103188189+AdarshSuryawansh@users.noreply.github.com>
2023-04-01 20:17:42 +0530

    Add files via upload

---
+++ Conditional Statements & Loops/23 Positive_Or_Nagative_Number_Approach_01.c
@@ -0,0 +1,28 @@
+#include<stdio.h>
+#include<conio.h>
+
+int main()
+{
+    int Num=0;
+
+    printf("\n====================*********========================");
+
+    printf("\n Enter Your Nummber=");
+    scanf("%d,&Num");
+
+    if(Num > 0)
+    {
+        print("\n\t %d is positive",Num);
+    }
+    else
+    {
+        printf("\n\t %d is Negative,Num");
+    }
+
+    printf("\n\n===================*********=========================");
+    printf("\n\n\t Thanks !!!!\n");
+
+    getch();
+    return 0;
+
+}
